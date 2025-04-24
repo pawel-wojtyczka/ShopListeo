74378af7efde099719997697e83900b6a41acbfe
Pawel Wojtyczka <pawel.wojtyczka@asperit.com>
2025-04-24 11:29:31 +0200

    Unit tests (basic) works

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,20 +1,25 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vitest/config";
+import { defineConfig, mergeConfig } from "vite";
+import { configDefaults } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: ["./src/__tests__/setup.ts"],
-    include: ["src/__tests__/unit/**/*.{test,spec}.{ts,tsx}"],
-    exclude: ["node_modules", ".astro", "dist"],
-    coverage: {
-      provider: "v8",
-      reporter: ["text", "json", "html"],
-      include: ["src/lib/**/*.{ts,tsx}"],
-      exclude: ["**/*.d.ts", "**/*.test.ts", "**/*.spec.ts"],
+export default mergeConfig(
+  defineConfig({
+    plugins: [react()],
+  }),
+  defineConfig({
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: ["./src/__tests__/setup.ts"],
+      include: ["src/__tests__/unit/**/*.{test,spec}.{ts,tsx}"],
+      exclude: [...configDefaults.exclude, ".astro", "dist"],
+      coverage: {
+        provider: "v8",
+        reporter: ["text", "json", "html"],
+        include: ["src/lib/**/*.{ts,tsx}"],
+        exclude: ["**/*.d.ts", "**/*.test.ts", "**/*.spec.ts"],
+      },
     },
-  },
-});
+  })
+);
