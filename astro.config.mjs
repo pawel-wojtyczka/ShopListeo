d5dd3d2f3b4bc95acf762ec39ea650bbaf1e553c
Pawel Wojtyczka <pawel.wojtyczka@asperit.com>
2025-04-30 11:34:08 +0200

    Change in astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
+  output: "server",
   integrations: [react(), sitemap()],
   server: { port: 3000 },
   vite: {
