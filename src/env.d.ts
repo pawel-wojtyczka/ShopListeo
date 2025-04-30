8d69f85366cda0e589c1247cd11a3b8090da422f
Pawel Wojtyczka <pawel.wojtyczka@asperit.com>
2025-04-30 14:03:29 +0200

    new master.yml

--- src/env.d.ts
+++ src/env.d.ts
@@ -21,10 +21,17 @@ declare global {
 
 export {};
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface ImportMetaEnv {
-  // Tutaj możesz dodać definicje innych zmiennych środowiskowych, jeśli są potrzebne
-  // np. readonly VITE_API_URL: string;
+  // Zmienne środowiskowe dla Supabase
+  readonly PUBLIC_SUPABASE_URL: string;
+  readonly PUBLIC_SUPABASE_ANON_KEY: string;
+
+  // Zmienne środowiskowe dla OpenAI
+  readonly OPENAI_API_KEY: string;
+
+  // Zmienne środowiskowe dla Cloudflare
+  readonly CLOUDFLARE_ACCOUNT_ID?: string;
+  readonly CLOUDFLARE_API_TOKEN?: string;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
