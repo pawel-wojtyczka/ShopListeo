2eb56bcaeee769db7893ad1f4b7bef8bacfa4ef2
Pawel Wojtyczka <pawel.wojtyczka@asperit.com>
2025-05-04 16:22:11 +0200

    Changes for app services

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
 
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
@@ -7,7 +8,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
   integrations: [react(), sitemap()],
   server: { port: 3000 },
   vite: {
